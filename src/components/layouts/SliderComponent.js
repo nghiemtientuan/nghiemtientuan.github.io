40f1b0040d9194ae70b1bb9221ffd4d4f9069df5
Nghiem Tuan <nghiem.tuan@tda.company>
2023-08-17 18:02:30 +0700

    feat: slides

--- src/components/layouts/SliderComponent.js
+++ src/components/layouts/SliderComponent.js
@@ -1,42 +1,71 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation, Autoplay } from 'swiper';
+import Lazyload from 'react-lazyload';
+import { useTranslation } from 'react-i18next';
+
+// Import Swiper styles
+import 'swiper/css';
+
+// Configs
+import { REACT_APP_URL_SLIDES } from '../../constants/Config';
 
 const SliderComponent = (_props) => {
   const slides = useSelector((state) => state.data.slides);
-  console.log(slides, 'slides');
+  const {t} = useTranslation();
 
   return (
     <div className="uk-container">
-      <div
-        className="uk-border-rounded-large uk-background-top-center uk-background-cover
-        uk-background-norepeat uk-light uk-inline uk-overflow-hidden uk-width-1-1"
-        style={{ backgroundImage: 'url(https://via.placeholder.com/1200x438.jpg)' }}
+      <Swiper
+        spaceBetween={2}
+        modules={[Pagination, Navigation, Autoplay]}
+        navigation={true}
+        loop={true}
+        pagination={{
+          dynamicBullets: true,
+          clickable: true,
+        }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
       >
-        <div className="uk-position-cover uk-header-overlay"></div>
-
-        <div className="uk-position-relative" data-uk-grid>
-          <div className="uk-width-1-2@m uk-flex uk-flex-middle">
-            <div className="uk-padding-large uk-padding-remove-right">
-              <h1 className="uk-heading-small uk-margin-remove-top">
-                Choose from thousands of recipes
-              </h1>
-              <p className="uk-text-secondary">
-                Appropriately integrate technically sound value with scalable infomediaries
-                negotiate sustainable strategic theme areas
-              </p>
-              <a
-                className="uk-text-secondary uk-text-600 uk-text-small hvr-forward"
-                href="sign-up.html"
+        {
+          slides.map((slide, slideIndex) => (
+            <SwiperSlide key={slideIndex}><span style={{opacity: 0}}>.</span>
+              <div
+                className="uk-border-rounded-large uk-background-top-center uk-background-cover
+          uk-background-norepeat uk-light uk-inline uk-overflow-hidden uk-width-1-1"
+                style={{ backgroundImage: `url(${slide})` }}
               >
-                Sign up today
-                <span className="uk-margin-small-left" data-uk-icon="arrow-right"></span>
-              </a>
-            </div>
-          </div>
+                <div className="uk-position-cover uk-header-overlay"></div>
 
-          <div className="uk-width-expand@m"></div>
-        </div>
-      </div>
+                <div className="uk-position-relative" data-uk-grid>
+                  <div className="uk-width-1-2@m uk-flex uk-flex-middle">
+                    <div className="uk-padding-large uk-padding-remove-right">
+                      <a
+                        className="uk-text-secondary uk-text-600 uk-text-small hvr-forward"
+                        href={REACT_APP_URL_SLIDES}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <Lazyload>
+                          <img src={slide} alt="slide"/>
+                        </Lazyload>
+                        {t('slides.register_now')}
+                        <span className="uk-margin-small-left" data-uk-icon="arrow-right"></span>
+                      </a>
+                    </div>
+                  </div>
+
+                  <div className="uk-width-expand@m"></div>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))
+        }
+      </Swiper>
     </div>
   );
 };
--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,4 +1,5 @@
 export const API_URL = process.env.REACT_APP_API_ENDPOINT;
+export const REACT_APP_URL_SLIDES = process.env.REACT_APP_URL_SLIDES;
 
 export const APP = {
   name: 'TinHocLapTrinh123',
